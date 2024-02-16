920e4d1438f7faca3c109eda381d16dd6b7814e1
nugrahaindie <nugraha@indietechsolutions.com>
2024-02-16 16:11:01 +0700

    perbaikan hello world

--- week-1/hello-world.py
+++ week-1/hello-world.py
@@ -1,1 +1,18 @@
 print("Hello World")
+
+# **Menghitung total_bayar**
+# jika belanja lebih dari 60 ribu dapat diskon 10.000
+
+# totalBelanja = ?
+# Berapa yang harus dibayar = ?
+
+# **Menghitung gaji**
+
+# gaji_pokok = ?
+# uang transport harian = 100.000/hari
+# uang makan = 50.000/hari 
+# uang lembur = 2 jam pertama itu 100.000/jam, jam berikutnya 150.000/jam
+
+# berapa honor yang saya dapat jika saya bekerja selama 21 hari dan lembur selama 10 jam 
+
+# honor = gaji_pokok+total_transport+total_makan+upah_lembur
